ec8a8c113478e4b99430ebd6538761715020a2ce
Osama Khan <138323797+osama0606@users.noreply.github.com>
2025-07-05 11:00:49 +0000

    Day1

---
+++ Day1/index.js
@@ -0,0 +1,40 @@
+
+const movies = ['3idiots','PK','Talash','Dhoom3','Dangle'];
+
+console.log(movies);
+
+movies.push('Dhoom2');
+
+console.log(movies);
+
+movies.shift();
+
+console.log(movies);
+
+movies.forEach((ele)=> console.log(ele));
+
+console.log(movies.includes('Dangle'))
+
+ 
+ 
+ 
+ 
+//  const str = 'OSAMA KHAN';
+
+// console.log(str);
+
+
+
+// // console.log(str.toUpperCase());
+
+// console.log(str.includes('osama'));
+
+// console.log(str.toLowerCase());
+
+// console.log(str.split(' '));
+
+// console.log(str.slice(0,5));
+
+// console.log(str.charAt(6));
+
+// console.log(str.indexOf('K'))
