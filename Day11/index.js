bb00f74322f5ad655b1ea92ded21a13f852de2ec
Osama Khan <138323797+osama0606@users.noreply.github.com>
2025-07-15 17:32:54 +0000

    Day11

---
+++ Day11/index.js
@@ -0,0 +1,30 @@
+//  merge sorted array
+
+
+function mergeSortedArr(arr1,arr2){
+    let merged = [];
+    let i=0, j=0;
+    while(i < arr1.length && j < arr2.length){
+        if(arr1[i] < arr2[j]){
+            merged.push(arr1[i]);
+            i++
+        }else{
+            merged.push(arr2[j]);
+            j++;
+        }
+    }
+    
+    while(i < arr1.length){
+        merged.push(arr1[i]);
+        i++;
+    }
+    
+    while(j < arr2.length){
+         merged.push(arr2[j]);
+         j++;
+    }
+    return merged;
+}
+
+console.log(mergeSortedArr([1, 3, 5], [2, 4, 6]));
+
