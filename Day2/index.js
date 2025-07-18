43ec0e669efefcc8af2423bc718c38147b64240f
Osama Khan <138323797+osama0606@users.noreply.github.com>
2025-07-18 05:59:13 +0000

    Day2

--- Day2/index.js
+++ Day2/index.js
@@ -1,62 +1,17 @@
 
-
-// const obj = {
-//     car:'mercedeez',
-//     brand:'gh3t',
-//     model:'42g255555',
-//     year:1999
-// }
-
-// for(let key in obj){
-//     console.log(key)
-// }
-
-
-
-// obj.color = 'red';
-
-// console.log(obj);
-
-// delete obj.year
-
-// console.log(obj);
-
-// console.log('model' in obj)
-
-
-function countCharacters(str) {
-  let map = {}; // empty hashmap (object)
-
-  for (let char of str) {
-    if (map[char]) {
-      map[char]++;  // if already exists, increase count
-    } else {
-      map[char] = 1; // if not exists, set 1
+function findMaxAndMin(arr){
+    let max = arr[0];
+    let min = arr[0];
+    
+    for(let i = 1; i < arr.length; i++){
+        if(arr[i] > max){
+            max = arr[i];
+        }
+        if(arr[i] < min){
+            min = arr[i];
+        }
     }
-  }
-
-  return map;
+    console.log(`The Maximum value is ${max} and Minimum value is ${min}. Differences is : ${max - min}`);
 }
 
-console.log(countCharacters("apple"));
-// Output: { a:1, p:2, l:1, e:1 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+findMaxAndMin([12, 7, 25, 3, 18]);
