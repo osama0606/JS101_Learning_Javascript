6d3980563c38ca44debac4721a46a5fe05d78afb
Osama Khan <138323797+osama0606@users.noreply.github.com>
2025-07-25 13:28:46 +0000

    Day9

--- Day9/index.js
+++ Day9/index.js
@@ -1,20 +1,9 @@
-// selection sort method
 
-
-function selectionSort(arr){
-    let n = arr.length;
-    
-    for(let i = 0; i < n-1; i++){
-        let midIndex = i;
-        for(let j = i + 1; j < n; j++){
-            if(arr[j] < arr[midIndex]){
-                midIndex = j;
-            }
-        }
-        // swap
-        [arr[i], arr[midIndex]] = [arr[midIndex],arr[i]]
+function reverseString(str){
+    let add = '';
+    for(let i = str.length-1; i >= 0; i--){
+        add = add + ' ' + str[i]
     }
-    return arr;
+    console.log(add)
 }
-
-console.log(selectionSort([4, 2, 7, 1, 3]));
+reverseString('osama')
