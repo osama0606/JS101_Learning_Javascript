34829028ee908303acc68f010df2fccfe7415d3b
Osama Khan <138323797+osama0606@users.noreply.github.com>
2025-07-27 07:21:08 +0000

    Day11

--- Day11/index.js
+++ Day11/index.js
@@ -1,30 +1,18 @@
-//  merge sorted array
-// jfsdfksdfjs
 
-function mergeSortedArr(arr1,arr2){
-    let merged = [];
-    let i=0, j=0;
-    while(i < arr1.length && j < arr2.length){
-        if(arr1[i] < arr2[j]){
-            merged.push(arr1[i]);
-            i++
-        }else{
-            merged.push(arr2[j]);
-            j++;
-        }
+function isAnagram(str1,str2){
+    let s1 = str1.replace(/\s/g,'').toLowerCase();
+    let s2 = str2.replace(/\s/g,'').toLowerCase();
+    
+    if(s1.length !== s2.length){
+        return false;
     }
     
-    while(i < arr1.length){
-        merged.push(arr1[i]);
-        i++;
-    }
+    let sort1 = s1.split('').sort().join('');
+    let sort2 = s2.split('').sort().join('');
     
-    while(j < arr2.length){
-         merged.push(arr2[j]);
-         j++;
-    }
-    return merged;
+    return sort1 === sort2;
 }
 
-console.log(mergeSortedArr([1, 3, 5], [2, 4, 6]));
+console.log(isAnagram('listen','silent'));
+console.log(isAnagram('hello','world'));
 
