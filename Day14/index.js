6783413066c4c99512f37b4e6601b4845dc573b1
Osama Khan <138323797+osama0606@users.noreply.github.com>
2025-07-30 16:53:14 +0000

    Day14

---
+++ Day14/index.js
@@ -0,0 +1,18 @@
+// first non-repeating character
+
+function firstNonRepeatingChar(str){
+    let freq = {};
+    
+    for(let i = 0; i < str.length; i++){
+        freq[str[i]] = (freq[str[i]] || 0) + 1;
+    }
+    for(let i = 0; i < str.length; i++){
+        if(freq[str[i]] === 1){
+            console.log(`first non-repeating character is ${str[i]}`);
+            return;
+        }
+    }
+    console.log(`No non-repeating character found`);
+}
+
+firstNonRepeatingChar('aabbccddee')
