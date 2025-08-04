4fca8dc261a0aef06dfc220d84f6be9dc2dbd912
Osama Khan <138323797+osama0606@users.noreply.github.com>
2025-08-04 02:08:38 +0000

    Day19

---
+++ Day19/index.js
@@ -0,0 +1,10 @@
+function pattern(){
+    for(let i = 1; i <= 4; i++){
+        let row = '';
+        for(let j = 1; j <= i; j++){
+            row += '* '
+        }
+        console.log(row)
+    }
+}
+pattern()
