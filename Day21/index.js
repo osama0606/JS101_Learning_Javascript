9d1b0308c1bff0dde07b73a876eb958c922ae851
Osama Khan <138323797+osama0606@users.noreply.github.com>
2025-08-06 02:58:26 +0000

    Day21

---
+++ Day21/index.js
@@ -0,0 +1,13 @@
+function rightAlignedTriangle(n){
+    for(let i = 1; i <= n; i++){
+        let row = '';
+        for(let j = 1; j <= n-i; j++){
+            row += ' ';
+        }
+        for(let k = 1; k <= i; k++){
+            row += '* ';
+        }
+        console.log(row)
+    }
+}
+rightAlignedTriangle(5);
