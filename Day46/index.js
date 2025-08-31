3894a86b32e9bed7e10cefba77e0a023d8597c2d
Osama Khan <138323797+osama0606@users.noreply.github.com>
2025-08-31 11:37:43 +0000

    Day46

---
+++ Day46/index.js
@@ -0,0 +1,29 @@
+class Stack {
+    constructor() {
+        this.items = [];
+    }
+    push(item) {
+        this.items.push(item);
+    }
+    pop() {
+        return this.items.pop();
+    }
+    peek() {
+        return this.items[this.items.length - 1];
+    }
+    isEmpty() {
+        return this.items.length === 0;
+    }
+    size() {
+        return this.items.length;
+    }
+}
+
+// Example usage:
+const stack = new Stack();
+stack.push(10);
+stack.push(20);
+console.log(stack.peek());    
+console.log(stack.pop());     
+console.log(stack.size());    
+console.log(stack.isEmpty());  
